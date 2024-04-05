7e706ff33228e04fd311f9203f03cac383ee3ea9
igalklebanov <igalklebanov@gmail.com>
2024-04-05 20:46:02 +0300

    yet another initial commit.

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-pkg'],
+}
