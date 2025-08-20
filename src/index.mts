ac58181ce2f6b93ff404511c9701838853d52728
igalklebanov <igalklebanov@gmail.com>
2025-08-21 01:23:28 +0300

    ...

---
+++ src/index.mts
@@ -0,0 +1,3 @@
+export * from './kyselify.mjs'
+
+console.warn('`kysely-supabase` should only be imported with the `type` annotation. It has no runtime exports, for now.')
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
+import { rename, unlink, watch } from 'node:fs/promises'
+import { join } from 'node:path'
 import {defineConfig} from 'tsup'
 
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['./src/index.ts'],
+  entry: ['./src/index.mts'],
   format: ['cjs', 'esm'],
-  legacyOutput: true,
-  outDir: 'dist',
-  sourcemap: true,
 })
